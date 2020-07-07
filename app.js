c07a16c289cda0cf4724cd20f22cd2956ce9bee9
Kaija Dunklin <kaykay_s@yahoo.com>
2020-07-07 17:24:23 -0400

    finished

--- app.js
+++ app.js
@@ -4,4 +4,23 @@ function addition ( kaija , keyon){
 return (kaija + keyon)}
 
 console.log(addition(kaija,keyon));
+function substraction (kaija,keyon){
+    return (kaija-keyon)
+}console.log(substraction(kaija,keyon))
+function multiply (kaija,keyon){
+    return (kaija * keyon)
+}console.log(multiply(kaija,keyon))
+function divide (kaija,keyon){
+    return (kaija / keyon)
+}console.log(divide(kaija,keyon))
 
+var kArray = [2,4,6,8,10];
+var tArray = [1,3,5,7,9];
+var sArray = [1,4,7,2,0];
+
+tArray.push(23);
+console.log(tArray)
+kArray.shift();
+console.log(kArray)
+sArray.pop();
+console.log(sArray)
